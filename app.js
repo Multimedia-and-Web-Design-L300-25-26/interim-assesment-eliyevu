4a4781a06ce53c8dc5a91a6b88a38836e71b1311
eliyevu <kancartos6@gmail.com>
2026-04-29 11:51:30 +0000

    modified

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ import cors from "cors";
 import pkg from "cookie-parser";
 import { notFound, errorHandler } from "./src/middleware/errorHandler.js";
 import authRoutes from "./src/routes/authRoutes.js";
+import cryptoRoutes from "./src/routes/cryptoRoutes.js";
 
 const cookieParser = pkg;
-
+// Initialize Express app
 const app = express();
 
 app.use(cors({ origin: true, credentials: true }));
@@ -18,8 +19,10 @@ app.get("/health", (req, res) => {
 });
 
 // Routes 
-app.use("/api", authRoutes) 
+app.use("/api", authRoutes); 
+app.use("/api/crypto", cryptoRoutes);
 
+// Error handling middleware  
 app.use(notFound);
 app.use(errorHandler);
 
